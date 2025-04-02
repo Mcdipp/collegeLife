f0b86aa0ce04358fe8ce1d9ed551d0290df1aec3
Manuel Dipp <manuel.dipp@viacesi.fr>
2025-04-02 14:43:46 +0200

    feat : added project packages and first classes

--- lwjgl3/src/main/java/com/manuel/collegeLife/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/manuel/collegeLife/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package com.manuel.collegeLife.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.manuel.collegeLife.MainGame;
+import com.manuel.collegeLife.game.MainGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
