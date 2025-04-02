c0ccb6aa6e9ea48f8e41ba9e0e380aa196c64421
Manuel Dipp <manuel.dipp@viacesi.fr>
2025-04-02 16:22:37 +0200

    feat : added movement for the player

--- core/src/main/java/com/manuel/collegeLife/entities/Player.java
+++ core/src/main/java/com/manuel/collegeLife/entities/Player.java
@@ -2,11 +2,14 @@ package com.manuel.collegeLife.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     private Texture texture;
     private Vector2 position;
+    private float speed = 200f;
 
     public Player(){
         texture = new Texture("player1.png");
@@ -22,6 +25,10 @@ public class Player {
     }
 
     public void update(float delta){
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) position.y += speed * delta;
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) position.y -= speed * delta;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) position.x -= speed * delta;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) position.x += speed * delta;
     }
 
     public void render(SpriteBatch batch){
